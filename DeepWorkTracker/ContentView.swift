c983d3fc82443bc23e5f651b24d8ba90bcc56267
Philip Krück <philip.krueck@apploft.de>
2020-04-13 14:22:46 +0200

    first commit

---
+++ DeepWorkTracker/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DeepWorkTracker
+//
+//  Created by Philip Krück on 13.04.20.
+//  Copyright © 2020 Philip Krück. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Deep Work Tracker")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
