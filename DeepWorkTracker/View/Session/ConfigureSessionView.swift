14b636819a820fbbcaaa751d83e75fafcd4355a0
Philip Krück <philip.krueck@apploft.de>
2020-04-29 14:07:10 +0200

    merged code from Add and EditSessionView

---
+++ DeepWorkTracker/View/Session/ConfigureSessionView.swift
@@ -0,0 +1,21 @@
+//
+//  ConfigureSessionView.swift
+//  DeepWorkTracker
+//
+//  Created by Philip Krück on 29.04.20.
+//  Copyright © 2020 Philip Krück. All rights reserved.
+//
+
+import SwiftUI
+
+struct ConfigureSessionView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ConfigureSessionView_Previews: PreviewProvider {
+    static var previews: some View {
+        ConfigureSessionView()
+    }
+}
